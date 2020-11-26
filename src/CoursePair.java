71243dcce7e5180a57dc9715558f695140bb3935
Mark Schoendorfer <mark.schoendorfer@ucalgary.ca>
2020-11-26 13:05:38 -0700

    initial commit

---
+++ src/CoursePair.java
@@ -0,0 +1,34 @@
+
+public class CoursePair {
+	
+	public final String EMPTY = "EMPTY";
+	
+	private String course;
+	private String timeslot;
+	
+	public CoursePair (String course) {
+		this.course = course;
+	}
+	
+	public CoursePair(String course, String timeslot) {
+		this.course = course;
+		this.timeslot = timeslot;
+	}
+	
+	public String getCourse() {
+		return course;
+	}
+	
+	public String getTime() {
+		return timeslot;
+	}
+	
+	public void schedule(String newTime) {
+		this.timeslot = newTime;
+	}
+	
+	public void unschedule() {
+		this.timeslot = EMPTY;
+	}
+	
+}
