63ec5e8cb8aa34e424aa52e31196214ee1565d06
Mark Schoendorfer <mark.schoendorfer@ucalgary.ca>
2020-11-26 13:28:41 -0700

    very rough idea

--- src/ProblemTree.java
+++ src/ProblemTree.java
@@ -1,4 +1,7 @@
 
 public class ProblemTree {
-
+	/****************************************************
+	 * Implement tree-like structure here. No need for backtracking, so no Erw*, but do need to implement Erw.
+	 * Concerns: ways to minimize memory use as tree expands. Depth-first search and only saving best result can maybe help?
+	 ****************************************************/
 }
