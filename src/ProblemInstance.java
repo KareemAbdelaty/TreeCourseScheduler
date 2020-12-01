a05e1ebf2a517e42bec9acdae6fb98c4d98edfb4
Mark Schoendorfer <mark.schoendorfer@ucalgary.ca>
2020-11-30 17:45:40 -0700

    added more detailed comments, expanded on constructors.

--- src/ProblemInstance.java
+++ src/ProblemInstance.java
@@ -2,28 +2,43 @@ import java.util.ArrayList;
 
 public class ProblemInstance {
 	
-	public final int BAD_CONSTR = 1;
-	public final int PART_CONSTR = 2;
-	public final int GOOD_CONSTR = 3;
+	public static final int BAD_CONSTR = 1; //schedule breaks some hard constraint
+	public static final int PART_CONSTR = 2; //schedule is incomplete (some classes unassigned) but otherwise breaks no constraints
+	public static final int GOOD_CONSTR = 3; //schedule is complete, breaks no constraints
 
 	private ArrayList<CoursePair> schedule;
+	private ArrayList<ProblemInstance> children;
+	private int evalScore;
 	
 	public ProblemInstance() {
-		
+		this.evalScore = -1;
+		//for each class, create an empty assignment.
 	} // if no partassign
 	
 	public ProblemInstance(ArrayList<CoursePair> partAssign) {
-		
+		this.evalScore = -1;
+		schedule.addAll(partAssign);
 	} // if partassign exists; once we decide on input format, maybe pass raw input and translate it into ArrayList here.
 	
-	public boolean Constr() {
+	public static ProblemInstance findSchedule(ProblemInstance pr) {
+		/**
+		 * Recursive; compute each possible schedule, then see if it at least meets PART_CONSTR. If so, add to children. Then, for each child, evaluate it's optimality (Eval()).
+		 * Once we evaluate, continue on from the most optimal to the least (I think we have to check each path until we have no valid solutions (they all break constr), but we might have to bound)
+		 **/
 		
-		return true;
-	} //maybe use typecode system? Breaks some constraint, good but not all courses scheduled, good and schedule complete?
+		return null;
+	}
+	
+	public int Constr() {
+		//evaluate constraints, return appropriate typecode. Problems: need to be able to compute efficiently for large problem sets. How to compare timeslots easily for Course/Tutorial conflicts?
+		return PART_CONSTR;
+	} 
 	
 	public int Eval() {
-		
-		return 1;
+		if(this.evalScore < 0) {
+			//compute eval, set evalScore to result. Prevents having to compute Eval multiple times.
+		}
+		return this.evalScore;
 	}
 }
 
--- src/ProblemTree.java
+++ src/ProblemTree.java
@@ -3,5 +3,8 @@ public class ProblemTree {
 	/****************************************************
 	 * Implement tree-like structure here. No need for backtracking, so no Erw*, but do need to implement Erw.
 	 * Concerns: ways to minimize memory use as tree expands. Depth-first search and only saving best result can maybe help?
-	 ****************************************************/
+	 * To reduce number of permutations at each step, schedule only one course at a time (ie if we have {(c1,e),(c2,e),(c3,e)} our next step will only consider the possible places to schedule c1.
+	 ***************************************************
+	 * Not sure we actually need this?????? just start with one Problem Instance at the head...
+	 **/
 }
